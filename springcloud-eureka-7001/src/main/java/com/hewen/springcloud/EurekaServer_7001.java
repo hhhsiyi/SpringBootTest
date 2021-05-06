cdc43de31e1ba530b75ead79a5b0128352eac8a6
hewen-Laptop <812309203@qq.com>
2021-05-06 20:53:19 +0800

    eureka

--- springcloud-eureka-7001/src/main/java/com/hewen/springcloud/EurekaServer_7001.java
+++ springcloud-eureka-7001/src/main/java/com/hewen/springcloud/EurekaServer_7001.java
@@ -2,13 +2,18 @@ package com.hewen.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 2021/5/6
+ * Hewen
+ * 云想衣裳花想容，春风拂槛露华浓
+ * 最是人间留不住，朱颜辞镜花辞树
+ */
 @SpringBootApplication
-@EnableEurekaClient//在服务启动后自动注册到eureka中
-public class DeptProvider_8001 {
+@EnableEurekaServer//服务端的启动类
+public class EurekaServer_7001 {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider_8001.class,args);
+        SpringApplication.run(EurekaServer_7001.class,args);
     }
 }
