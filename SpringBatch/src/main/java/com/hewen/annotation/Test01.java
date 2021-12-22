f4632585c62e9c6a1f0108c88fcd179d249046e4
hewen-PC <812309203@qq.com>
2021-12-23 00:09:29 +0800

    +++

---
+++ SpringBatch/src/main/java/com/hewen/annotation/Test01.java
@@ -0,0 +1,13 @@
+package com.hewen.annotation;
+
+public class Test01 extends Object{
+    //什么是注解
+
+    @Override //重写
+    public String toString() {
+        return super.toString();
+    }
+    public void testA(){
+
+    }
+}
