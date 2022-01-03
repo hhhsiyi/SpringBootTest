2f8aa32a152597884625c5c8dbe3d0215e771ba6
hewen-PC <812309203@qq.com>
2022-01-04 00:16:47 +0800

    分布式锁

---
+++ redis-lock/src/main/java/com/hewen/RedisLockApplication.java
@@ -0,0 +1,30 @@
+package com.hewen;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * ClassName com.hewen.RedisLockApplication
+ * Description
+ * Create by Hewen
+ * 早知如此绊人心，何如当初莫相识
+ * Date 2022/1/3 12:03
+ */
+@SpringBootApplication
+public class RedisLockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLockApplication.class, args);
+    }
+//nginx反向代理之后
+    @Bean
+    public Redisson redisson() {
+        Config config = new Config();
+//        单机
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+        return (Redisson) Redisson.create(config);
+    }
+
+}
